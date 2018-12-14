239c8b42c053c2cc1bcda902a2cd4f807cfadf86
Devin ONeill <devinoneill1@gmail.com>
2018-12-13 21:10:49 -0500

    Second Problem Done

--- main.js
+++ main.js
@@ -6,14 +6,21 @@ function isLeapYear(year) {
 console.log(isLeapYear('2012')) // returns true 
 console.log(isLeapYear('2010')) // returns false
 // Code for Part Two 
+function vowelOrCons(letterIn) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    if (vowels.includes(letterIn.toLowerCase())) {
+        return ('vowel')
+    } else if (letterIn === 'y') {
+        return ('vowel or consonant')
+    } else {
+        return ('consonant')
+    }
 
+}
 
-
-
-
-/* vowelOrCons('a') // return 'vowel'
-vowelOrCons('y') // return 'vowel or consonant'
-vowelOrCons('h') // return 'consonant'
+console.log(vowelOrCons('a')) // return 'vowel'
+console.log(vowelOrCons('y')) // return 'vowel or consonant'
+console.log(vowelOrCons('h')) // return 'consonant'
 
 // Code for Part Three 
 
@@ -22,4 +29,4 @@ vowelOrCons('h') // return 'consonant'
 
 
 
-daysInMonth('December') // returns 31 */
+//daysInMonth('December') // returns 31
